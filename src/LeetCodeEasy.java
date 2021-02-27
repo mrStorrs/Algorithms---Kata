dee453ac830f2939f8cf90cbcdeff8c11fe2aecb
mrStorrs <cj.storrs@gmail.com>
2021-02-26 20:58:57 -0800

    +1 LeetCodeEasy

--- src/LeetCodeEasy.java
+++ src/LeetCodeEasy.java
@@ -48,4 +48,75 @@ public class LeetCodeEasy {
         }
         return revLong.intValue(); 
     }
+
+    /*
+     * #11 Roman To Integer
+     * completed 2/26/2020 (99% more efficent than others! & 99% less memory)
+     * 
+     * For example, 2 is written as II in Roman numeral, just two one's added
+     * together. 12 is written as XII, which is simply X + II. The number 27 is
+     * written as XXVII, which is XX + V + II.
+     * 
+     * Roman numerals are usually written largest to smallest from left to right.
+     * However, the numeral for four is not IIII. Instead, the number four is
+     * written as IV. Because the one is before the five we subtract it making four.
+     * The same principle applies to the number nine, which is written as IX. There
+     * are six instances where subtraction is used:
+     * 
+     * I can be placed before V (5) and X (10) to make 4 and 9. X can be placed
+     * before L (50) and C (100) to make 40 and 90. C can be placed before D (500)
+     * and M (1000) to make 400 and 900. Given a roman numeral, convert it to an
+     * integer.
+     * 
+     * test cases: System.out.println(LeetCodeEasy.romanToInt("III"));
+     */
+    public static int romanToInt(String s) {
+        char c; //holds current char
+        int sum = 0; //holds running sum of #'s
+        for(int i = 0; i < s.length(); i++){
+            c = s.charAt(i); //set current char
+            switch(c){
+                case 'I': 
+                    if(i < s.length() -1){
+                        if(s.charAt(i+1) == 'X' || s.charAt(i+1) == 'V'){
+                            sum --;
+                            break; //don't add the number
+                        } 
+                    }
+                    sum++;
+                    break;
+                case 'V':
+                    sum += 5; 
+                    break;
+                case 'X':
+                    if (i < s.length() - 1) {
+                        if (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C') {
+                            sum -= 10; // don't add the number
+                            break;
+                        }
+                    }
+                    sum += 10;
+                    break;
+                case 'L':
+                    sum += 50;
+                    break;
+                case 'C':
+                    if (i < s.length() - 1) {
+                        if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M') {
+                            sum -= 100; // don't add the number
+                            break;
+                        }
+                    } 
+                    sum += 100; 
+                    break;
+                case 'D':
+                    sum += 500;
+                    break;
+                case 'M':
+                    sum += 1000; 
+                    break;
+            }
+        }
+        return sum;
+    }    
 }
